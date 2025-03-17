dc791f785b7b695d2b57981ff8ac585e567e8335
gemy3731 <mohamedgamalz3731@gmail.com>
2025-03-17 10:29:31 +0200

    feat(reviews): add modal to reviews

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,5 +1,6 @@
+import { Modal } from "flowbite-react";
 import { useState } from "react";
-
+import { IoAddCircleSharp } from "react-icons/io5";
 interface ItemType {
   id: number;
   name: string;
@@ -61,6 +62,12 @@ const list = [
 
 const Reviews = () => {
   const [reviews, setReviews] = useState<ItemType[]>(list);
+  const [openModal, setOpenModal] = useState(false);
+
+  function onCloseModal() {
+    setOpenModal(false);
+  }
+
   const handleDelete = (e: number) => {
     console.log(e);
     setReviews(reviews.filter((review) => review.id !== e));
@@ -71,57 +78,70 @@ const Reviews = () => {
         <h2 className="text-[32px] text-center font-[400] leading-[29.05px] mb-[26px]">
           Reviews
         </h2>
-        <div className="border rounded-[8px] pt-[42px] px-[27px] pb-[25px]">
-          <h3 className="text-[24px] text-center font-[400] leading-[29.05px] mb-[26px]">
-            Add New Review
-          </h3>
-          <form className="flex flex-col gap-4 dashFrom">
-            <label htmlFor="reviews-Description" className="block">
-              Reviews Description
-            </label>
-            <textarea
-              id="reviews-Description"
-              placeholder="Reviews Description"
-              className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
-            />
-            <label htmlFor="reviews-name" className="block">
-              Name
-            </label>
-            <input
-              type="text"
-              id="reviews-name"
-              placeholder="Name"
-              className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
-            />
-            <label htmlFor="reviews-role" className="block">
-              Role
-            </label>
-            <input
-              type="text"
-              id="reviews-role"
-              placeholder="Role"
-              className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
-            />
-            <label htmlFor="reviews-rating" className="block">
-              Rating
-            </label>
-            <input
-              type="number"
-              id="reviews-rating"
-              placeholder="Rating"
-              min={0}
-              max={5}
-              className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
-            />
-            <button className="bg-[#FF9900] rounded-[8px] px-[44px] py-[8px] text-white w-fit mx-auto">
-              Add Review
-            </button>
-          </form>
-        </div>
+
+        <Modal
+          show={openModal}
+          size="6xl"
+          onClose={onCloseModal}
+          popup
+          className="bg-black"
+        >
+          <Modal.Header className="bg-[#edeeee]" />
+          <Modal.Body className="bg-[#edeeee]">
+            <div className="border rounded-[8px] pt-[42px] px-[27px] pb-[25px] bg-white">
+              <h3 className="text-[24px] text-center font-[400] leading-[29.05px] mb-[26px]">
+                Add New Review
+              </h3>
+              <form className="flex flex-col gap-4 dashFrom">
+                <label htmlFor="reviews-Description" className="block">
+                  Reviews Description
+                </label>
+                <textarea
+                  id="reviews-Description"
+                  placeholder="Reviews Description"
+                  className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
+                />
+                <label htmlFor="reviews-name" className="block">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="reviews-name"
+                  placeholder="Name"
+                  className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
+                />
+                <label htmlFor="reviews-role" className="block">
+                  Role
+                </label>
+                <input
+                  type="text"
+                  id="reviews-role"
+                  placeholder="Role"
+                  className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
+                />
+                <label htmlFor="reviews-rating" className="block">
+                  Rating
+                </label>
+                <input
+                  type="number"
+                  id="reviews-rating"
+                  placeholder="Rating"
+                  min={0}
+                  max={5}
+                  className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
+                />
+                <button
+                  onClick={() => setOpenModal(false)}
+                  className="bg-[#FF9900] flex items-center gap-2  rounded-[8px] px-[44px] py-[8px] text-white w-fit mx-auto"
+                >
+                  <IoAddCircleSharp className="text-[18px]" /> Add Review
+                </button>
+              </form>
+            </div>
+          </Modal.Body>
+        </Modal>
+
         <div className="border rounded-[8px] pt-[42px] px-[27px] pb-[25px] mt-10">
-          <h3 className="text-[24px] text-center font-[400] leading-[29.05px] mb-[26px]">
-            All Reviews
-          </h3>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {reviews.map((review) => {
               return (
@@ -156,6 +176,12 @@ const Reviews = () => {
             })}
           </div>
         </div>
+        <button
+          onClick={() => setOpenModal(true)}
+          className="bg-[#FF9900] mt-4 flex items-center gap-2 rounded-[8px] px-[44px] py-[8px] text-white w-fit mx-auto"
+        >
+          <IoAddCircleSharp className="text-[18px]" /> Add Review
+        </button>
       </div>
     </>
   );
