4ab2edf387eb2162baf47b40ea5b8c25734478b1
Mohamed Gamal <mohamedgamalz3731@gmail.com>
2025-04-14 18:06:49 +0200

    feat(aboutus): make images as string link

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -9,13 +9,12 @@ interface AboutUsData {
   left_card_lower_text: string;
   right_card_upper_text: string;
   right_card_lower_text: string;
-  left_card_image: string | null;
-  right_card_image: string | null;
+  left_card_image: string;
+  right_card_image: string;
 }
 const AboutUs = () => {
   const [aboutUs, setAboutUs] = useState<AboutUsData>();
-  const [fileNameRight, setFileNameRight] = useState("");
-  const [fileNameLeft, setFileNameLeft] = useState("");
+
 
   useEffect(() => {
     getData();
@@ -29,65 +28,27 @@ const AboutUs = () => {
   };
   const formik = useFormik({
     initialValues: {
-      desc: aboutUs?.description || "",
-      upperTextLeftCard: aboutUs?.left_card_upper_text || "",
-      lowerTextLeftCard: aboutUs?.left_card_lower_text || "",
-      upperTextRightCard: aboutUs?.right_card_upper_text || "",
-      lowerTextRightCard: aboutUs?.right_card_lower_text || "",
-      ImgLeftCard: null,
-      ImgRightCard: null,
+      description: aboutUs?.description || "",
+      left_card_upper_text: aboutUs?.left_card_upper_text || "",
+      left_card_lower_text: aboutUs?.left_card_lower_text || "",
+      right_card_upper_text: aboutUs?.right_card_upper_text || "",
+      right_card_lower_text: aboutUs?.right_card_lower_text || "",
+      left_card_image: aboutUs?.left_card_image ||"" ,
+      right_card_image: aboutUs?.right_card_image||"",
     },
     enableReinitialize: true,
     onSubmit: (values) => {
       console.log(values);
-      const formData = new FormData();
-      formData.append("description", values.desc);
-      formData.append("left_card_upper_text", values.upperTextLeftCard);
-      formData.append("left_card_lower_text", values.lowerTextLeftCard);
-      formData.append("right_card_upper_text", values.upperTextRightCard);
-      formData.append("right_card_lower_text", values.lowerTextRightCard);
-      if (values.ImgLeftCard) {
-        formData.append("left_card_image", values.ImgLeftCard);
-      }
-      if (values.ImgRightCard) {
-        formData.append("right_card_image", values.ImgRightCard);
-      }
-      addNewAbout(formData);
+      addNewAbout(values);
     },
   });
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.name);
-    if (event.target.name === "ImgLeftCard") {
-      if (event.target.files) {
-        const selectedFile = event.target.files[0];
-        setFileNameLeft(selectedFile ? selectedFile.name : "");
-        formik.setFieldValue("ImgLeftCard", selectedFile);
-        console.log(selectedFile);
-      }
-    } else {
-      if (event.target.files) {
-        const selectedFile = event.target.files[0];
-        setFileNameRight(selectedFile ? selectedFile.name : "");
-        formik.setFieldValue("ImgRightCard", selectedFile);
-        console.log(selectedFile);
-      }
-    }
-  };
 
-  function addNewAbout(formData: FormData) {
-    const jsonData = {
-      description: formData.get("description"),
-      left_card_upper_text: formData.get("left_card_upper_text"),
-      left_card_lower_text: formData.get("left_card_lower_text"),
-      right_card_upper_text: formData.get("right_card_upper_text"),
-      right_card_lower_text: formData.get("right_card_lower_text"),
-      left_card_image: formData.get("left_card_image"),
-      right_card_image: formData.get("right_card_image"),
-    };
-  
+
+  function addNewAbout(values: AboutUsData) {
+
     fetch(`${apiUrl}/api/about`, {
       method: "POST",
-      body: JSON.stringify(jsonData),
+      body: JSON.stringify(values),
       headers: {
         "Content-Type": "application/json"
       },
@@ -122,7 +83,7 @@ const AboutUs = () => {
               name="desc"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.desc}
+              value={formik.values.description}
               className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
             />
             <label htmlFor="Upper-text-in-left-card" className="block">
@@ -135,7 +96,7 @@ const AboutUs = () => {
               name="upperTextLeftCard"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.upperTextLeftCard}
+              value={formik.values.left_card_upper_text}
               className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
             />
             <label htmlFor="Lower-text-in-left-card" className="block">
@@ -148,7 +109,7 @@ const AboutUs = () => {
               name="lowerTextLeftCard"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.lowerTextLeftCard}
+              value={formik.values.left_card_lower_text}
               className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
             />
             <label htmlFor="Upper-text-in-right-card" className="block">
@@ -161,7 +122,7 @@ const AboutUs = () => {
               name="upperTextRightCard"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.upperTextRightCard}
+              value={formik.values.right_card_upper_text}
               className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
             />
             <label htmlFor="Lower-text-in-right-card" className="block">
@@ -174,59 +135,36 @@ const AboutUs = () => {
               name="lowerTextRightCard"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.lowerTextRightCard}
+              value={formik.values.right_card_lower_text}
               className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
             />
-            <label htmlFor="file-input-right" className="block">
+            <label htmlFor="right_card_image" className="block">
               Image in right card
             </label>
-            <div className="file-upload">
               <input
                 type="text"
-                value={fileNameRight}
-                className="order-2"
-                readOnly
+                name="right_card_image"
+                id="right_card_image"
+                value={formik.values.right_card_image}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
                 placeholder="Image in right card"
               />
-              <input
-                type="file"
-                name="ImgRightCard"
-                accept="image/*"
-                id="file-input-right"
-                onChange={handleFileChange}
-              />
-              <label
-                htmlFor="file-input-right"
-                className="file-button order-1 md:order-3"
-              >
-                Choose File
-              </label>
-            </div>
-            <label htmlFor="file-input-left" className="block">
+
+            <label htmlFor="left_card_image" className="block">
               Image in left card
             </label>
-            <div className="file-upload">
               <input
                 type="text"
-                value={fileNameLeft}
-                className="order-2"
-                readOnly
+                name="left_card_image"
+                id="file-input-left"
+                value={formik.values.left_card_image}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className="outline-[#D1D1D1DD] border-[#D1D1D1DD] w-full rounded-[8px]"
                 placeholder="Image in left card"
               />
-              <input
-                type="file"
-                name="ImgLeftCard"
-                accept="image/*"
-                id="file-input-left"
-                onChange={handleFileChange}
-              />
-              <label
-                htmlFor="file-input-left"
-                className="file-button order-1 md:order-3"
-              >
-                Choose File
-              </label>
-            </div>
             <button
               type="submit"
               className="bg-[#FF9900] rounded-[8px] px-[44px] py-[8px] text-white w-fit mx-auto"
